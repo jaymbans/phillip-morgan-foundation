9456465a972de2c022c134214087b6ac80538acd
jay mbans <jordan@mbanefo.com>
2023-05-25 19:09:00 -0400

    responsive navigation bar structure

--- philip-morgan-foundation/src/App.js
+++ philip-morgan-foundation/src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 
 
 function App() {
+
   return (
     <div className="App">
       <NavBar />
--- philip-morgan-foundation/src/components/NavBar.jsx
+++ philip-morgan-foundation/src/components/NavBar.jsx
@@ -3,6 +3,9 @@ import './_Navbar.scss'
 import blueLogo from '../media/blue_small_logo.png'
 
 function Navbar() {
+  const activateHamburgerMenu = (e) => {
+    console.log(e)
+  }
 
   return (
     <nav>
@@ -17,9 +20,13 @@ function Navbar() {
         <button>ABOUT</button>
         <button>EVENTS</button>
         <button>CONTACT</button>
+        <button>DONATE</button>
       </div>
       <div className="nav-right">
         <button>GIVE</button>
+        <span className='bar'></span>
+        <span className='bar'></span>
+        <span className='bar'></span>
       </div>
     </nav>
   )
