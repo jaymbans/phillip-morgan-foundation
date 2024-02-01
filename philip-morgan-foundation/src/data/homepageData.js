61068d9e43c5fe2c6337b4bb2ada1879605dd456
jay mbans <jordan@mbanefo.com>
2024-02-01 01:28:51 -0500

    kept only financial help on homepage

--- philip-morgan-foundation/src/data/homepageData.js
+++ philip-morgan-foundation/src/data/homepageData.js
@@ -4,29 +4,29 @@ export const waysWehelp = [
     description: `Providing critical financial support to those in need, our organization helps alleviate the burden of expenses, ensuring individuals and families can access essential resources.`,
     icon: 'money'
   },
-  {
-    title: 'Meal Assistance',
-    description: `We believe no one should go hungry. Through our meal assistance program, we provide nourishing food to those facing food insecurity, fostering a healthier and more vibrant community.`,
-    icon: 'food'
-  },
-  {
-    title: 'Home Volunteers',
-    description: `Our dedicated team of volunteers extends a helping hand to enhance the quality of life for individuals in their homes. From companionship to practical assistance, we're here to support every step of the way.`,
-    icon: 'volunteer'
-  },
-  {
-    title: 'Holiday and Birthday Preparations',
-    description: `Celebrating special moments is a fundamental part of life. Our organization ensures that everyone, regardless of their circumstances, can experience the joy of holidays and birthdays through thoughtful preparations and festivities.`,
-    icon: 'cake'
-  },
-  {
-    title: 'Transportation',
-    description: `Bridging the gap in accessibility, our transportation assistance program ensures individuals can reach vital destinations, fostering independence and removing barriers to essential services.`,
-    icon: 'transportation'
-  },
-  {
-    title: 'Medical Supplies',
-    description: `Health is a priority, and our organization is committed to providing necessary medical supplies to those who may face challenges in obtaining them. Ensuring access to healthcare is a cornerstone of our mission.`,
-    icon: 'medical'
-  },
+  // {
+  //   title: 'Meal Assistance',
+  //   description: `We believe no one should go hungry. Through our meal assistance program, we provide nourishing food to those facing food insecurity, fostering a healthier and more vibrant community.`,
+  //   icon: 'food'
+  // },
+  // {
+  //   title: 'Home Volunteers',
+  //   description: `Our dedicated team of volunteers extends a helping hand to enhance the quality of life for individuals in their homes. From companionship to practical assistance, we're here to support every step of the way.`,
+  //   icon: 'volunteer'
+  // },
+  // {
+  //   title: 'Holiday and Birthday Preparations',
+  //   description: `Celebrating special moments is a fundamental part of life. Our organization ensures that everyone, regardless of their circumstances, can experience the joy of holidays and birthdays through thoughtful preparations and festivities.`,
+  //   icon: 'cake'
+  // },
+  // {
+  //   title: 'Transportation',
+  //   description: `Bridging the gap in accessibility, our transportation assistance program ensures individuals can reach vital destinations, fostering independence and removing barriers to essential services.`,
+  //   icon: 'transportation'
+  // },
+  // {
+  //   title: 'Medical Supplies',
+  //   description: `Health is a priority, and our organization is committed to providing necessary medical supplies to those who may face challenges in obtaining them. Ensuring access to healthcare is a cornerstone of our mission.`,
+  //   icon: 'medical'
+  // },
 ]
