5095bccf6d217fee2e912ad92885c5a0a9b12a71
jay mbans <jordan@mbanefo.com>
2024-02-12 17:43:13 -0500

    updated homepage link to go to cornhole event

--- philip-morgan-foundation/src/PageComponents/Home/Home.jsx
+++ philip-morgan-foundation/src/PageComponents/Home/Home.jsx
@@ -62,7 +62,7 @@ function Home() {
               })
             }
           </div>
-          <Link to='/events' className="blue btn">learn more</Link>
+          <Link to='/cornhole-event' className="blue btn">learn more</Link>
         </div>
       </section>
     </div>
