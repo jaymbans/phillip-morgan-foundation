373a4b0dbd92a6f23eea8555b471b60b8d89f25b
jay mbans <jordan@mbanefo.com>
2025-01-13 22:50:17 -0500

    update total donation to 33k

--- philip-morgan-foundation/src/data/homepageData.js
+++ philip-morgan-foundation/src/data/homepageData.js
@@ -1,7 +1,7 @@
 export const waysWehelp = [
   {
     title: 'Financial Assistance',
-    description: `Providing critical financial support to those in need, our organization helps alleviate the burden of expenses, ensuring individuals and families can access essential resources. We have donated over $20,000 to families in need to this date.`,
+    description: `Providing critical financial support to those in need, our organization helps alleviate the burden of expenses, ensuring individuals and families can access essential resources. We have donated over $33,000 to families in need to this date.`,
     icon: 'money'
   },
 ]
