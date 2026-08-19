cb47dd461d615606d38d53d8f5fe79c2a5e23402
jaymbans <jordan@mbanefo.com>
2026-08-19 16:36:23 -0400

    update total donations

--- pmf-app/src/pages/Home.jsx
+++ pmf-app/src/pages/Home.jsx
@@ -59,7 +59,7 @@ function Home() {
             <CardDescription className="text-base leading-relaxed text-white/80">
               Providing critical financial support to those in need, our organization helps
               alleviate the burden of expenses, ensuring individuals and families can access
-              essential resources. We have donated over $33,000 to families in need to this
+              essential resources. We have donated over $55,000 to families in need to this
               date.
             </CardDescription>
           </CardContent>
